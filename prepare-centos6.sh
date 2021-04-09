0aeea4af271ce5a2193b245495d87604d27315e7
Philipp Wagner <phw@lowrisc.org>
2021-04-09 12:11:47 +0100

    Build toolchains in a CentOS6 container
    CentOS6/RHEL6 is old, but still used in the EDA industry. Building our
    cross-compilation toolchain with the oldest possible distribution allows
    us to produce binaries that work on CentOS6/RHEL6 and all more recent
    distributions up to today.
    
    Based on earlier work by Sam Elliott, which was based on Holy Build Box.
    Unfortunately, HBB version 2.2 (based on CentOS6) does not provide a
    version of git with support for HTTPS remotes, which is something we
    need to check out further repositories.
    
    Instead, use a slightly adapted version of the manylinux2010 image,
    which is designed to build Python wheel (binary) packages. We need to
    slightly adapt it to add sudo to it, which is required for Azure
    Pipelines to work.
    
    Signed-off-by: Philipp Wagner <phw@lowrisc.org>

---
+++ prepare-centos6.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Copyright lowRISC contributors.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Install prerequisite packages.
+#
+# Notes:
+# - The devtoolset-8 SCL is already installed as part of the base image and
+#   provides a sufficiently new GCC version.
+# - Python 2.7 or newer is required by the LLVM build system.
+sudo yum install -y \
+  git \
+  shadow-utils \
+  bison \
+  flex \
+  texinfo \
+  help2man \
+  gawk \
+  gettext \
+  wget \
+  curl \
+  xz \
+  ncurses-devel \
+  ncurses-static \
+  pixman-devel \
+  rh-python35 \
+  zlib-devel \
+  zlib-static \
+  libffi-devel
+
+# Enable SCLs for all shell sessions.
+# See https://access.redhat.com/solutions/527703
+cat << EOF > /tmp/scl_enable.sh
+#!/bin/bash
+source scl_source enable rh-python35
+EOF
+sudo mv /tmp/scl_enable.sh /etc/profile.d/scl_enable.sh
