efdb0b1e775730aa72e5a52b7499d4955b30c607
Luís Marques <luismarques@lowrisc.org>
2022-01-13 12:55:52 +0000

    Toolchain updates for bitmanip 1.0+0.93 support
    For all toolchains:
    
    - GCC 10.20
    - binutils 2.35
    
    For the bitmanip toolchains (rv32imcb):
    
    - GCC and binutils with bitmanip 1.0+0.93 forks/patches
    - LLVM 13.0.1-rc3. Supports most of bitmanip 0.93 with 1.0-compatible encodings.
      The only exception is the shift ones instructions (slo, sloi, sro, sroi).

--- sw-versions.sh
+++ sw-versions.sh
@@ -12,3 +12,6 @@ export QEMU_VERSION=23967e5b2a6c6d04b8db766a8a149f3631a7b899
 # master 2020-05-25
 export LLVM_URL=https://github.com/llvm/llvm-project.git
 export LLVM_VERSION=fa038e03504c7d0dfd438b1dfdd6da7081e75617
+
+# LLVM 13.0.1-rc3 (tag llvmorg-13.0.1-rc3)
+export LLVM_BITMANIP_VERSION=75e33f71c2dae584b13a7d1186ae0a038ba98838
