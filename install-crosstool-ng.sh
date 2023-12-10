4e44e0c75866ff1c725c7fda359f9823bbc839ff
James Wainwright <james.wainwright@lowrisc.org>
2023-12-10 13:10:20 +0000

    Move CURSES_LIBS workaround to Azure yaml
    This allows _build-deps.yml to be Centos6-specific, while
    `install-crosstool-ng.sh` can be called from other environments.

--- install-crosstool-ng.sh
+++ install-crosstool-ng.sh
@@ -20,11 +20,4 @@ git checkout --force "${CROSSTOOL_NG_VERSION}"
 
 ./bootstrap
 
-# Explicitly include libtinfo in the ncurses linker options since it seems to be
-# not picked up by configure itself, leading to the following error otherwise:
-#
-# /usr/local/bin/libtool  --tag CC  --mode=link gcc  -g -O2   -o nconf nconf-nconf.o nconf-nconf.gui.o nconf-zconf.o -lmenuw -lpanelw -lncursesw
-# libtool: link: gcc -g -O2 -o nconf nconf-nconf.o nconf-nconf.gui.o nconf-zconf.o  -lmenuw -lpanelw -lncursesw
-# /opt/rh/devtoolset-8/root/usr/libexec/gcc/x86_64-redhat-linux/8/ld: nconf-nconf.o: undefined reference to symbol 'keypad'
-# //lib64/libtinfo.so.5: error adding symbols: DSO missing from command line
-CURSES_LIBS="-lcursesw -ltinfo" ./configure --prefix=/usr/local && make && sudo make install
+./configure --prefix=/usr/local && make && sudo make install
