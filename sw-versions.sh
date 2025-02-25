7d2bc4229c8429cdbecde2909e7484974218cde8
Luís Marques <luismarques@lowrisc.org>
2025-02-25 18:20:06 +0000

    [WIP] Test LLVM 20

--- sw-versions.sh
+++ sw-versions.sh
@@ -9,10 +9,6 @@ export CROSSTOOL_NG_VERSION=5a09578b6798f426b62d97b2ece1ba5e7b82990b
 # v4.0.1
 export QEMU_VERSION=23967e5b2a6c6d04b8db766a8a149f3631a7b899
 
-# LLVM 16.0.2 plus:
-# - hardening patches
-# - unratified bitmanip extensions
-# - `.option arch` assembly directive
-export LLVM_URL=https://github.com/lowRISC/llvm-project.git
-export LLVM_BRANCH=ot-llvm-16-hardening
-export LLVM_VERSION=d213f6b2e0bcc561930538ef47b3b5cc900b5b17
+export LLVM_URL=https://github.com/llvm/llvm-project.git
+export LLVM_BRANCH=main
+export LLVM_VERSION=4f18f3f09a744ddd05de2188592fa11533ff3054 # LLVM 20
