7e1e31484150b3b4573f4481d9cb09671e06de7c
James Wainwright <james.wainwright@lowrisc.org>
2025-07-17 14:14:52 +0100

    Build binutils without crosstool-ng

--- sw-versions.sh
+++ sw-versions.sh
@@ -17,3 +17,8 @@ export QEMU_VERSION=23967e5b2a6c6d04b8db766a8a149f3631a7b899
 export LLVM_URL=https://github.com/lowRISC/llvm-project.git
 export LLVM_BRANCH=ot-llvm-16-hardening
 export LLVM_VERSION=dec908d48facb6041c12b95b8ade64719a894917
+
+# RISC-V fork of Binutils 2.35 with bitmanip instruction support
+export BINUTILS_URL=https://github.com/riscv-collab/riscv-binutils-gdb.git
+export BINUTILS_BRANCH=riscv-binutils-2.35-rvb
+export BINUTILS_COMMIT=7c9dd840fbb6a1171a51feb08afb859288615137
