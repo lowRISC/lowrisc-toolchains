4c138ec9141d2e068f72321e294d0577d9e230e1
James Wainwright <james.wainwright@lowrisc.org>
2025-08-13 16:32:12 +0100

    Change Clang args to be named
    Signed-off-by: James Wainwright <james.wainwright@lowrisc.org>

--- contrib/build-opentitan-dist.sh
+++ contrib/build-opentitan-dist.sh
@@ -5,12 +5,12 @@ set -ex
 ./build-binutils.sh "riscv32-unknown-elf"
 
 ./build-clang-with-args.sh \
-  "lowrisc-toolchain-rv32imcb" \
-  "Release" \
-  "riscv32-unknown-elf" \
-  "rv32imc_zba_zbb_zbc_zbs" \
-  "ilp32" \
-  "medany"
+  --name "lowrisc-toolchain-rv32imcb" \
+  --target "riscv32-unknown-elf" \
+  --march "rv32imc_zba_zbb_zbc_zbs" \
+  --mabi "ilp32" \
+  --mcmodel "medany" \
+  "$@"
 
 ./create-prefixed-archive.sh \
   "lowrisc-toolchain-rv32imcb" \
